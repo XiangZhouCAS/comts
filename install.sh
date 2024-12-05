3e9f8aef9e62ef25ee6ec378897166fc4f636895
XiangZhouCAS <147217990+XiangZhouCAS@users.noreply.github.com>
2024-12-05 09:11:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 chmod +x comts
-chmod +x com
-chmod +x custom
+chmod +x ./bins/com
+chmod +x ./bins/custom
 comts_path=$(dirname "$(readlink -f "$0")")
 echo 'export PATH="$PATH:'"$comts_path"'"' >> ~/.bashrc
 bins_dir=$comts_path/$(echo bins)
