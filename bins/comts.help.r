73b3039a0f47c4c9f4b589cbb65bc1f6e3d8fb83
XiangZhouCAS <147217990+XiangZhouCAS@users.noreply.github.com>
2024-12-10 11:28:54 +0800

    Update comts.help.r

--- bins/comts.help.r
+++ bins/comts.help.r
@@ -7,7 +7,7 @@ option_list <- list(
   make_option(c("--help","-h"),type = "character",default = F,
               help = "Show this help message and exit"))
 opt_parser = OptionParser(
-  usage = "usage: comts geneset	To calculate community and RPKM abundance of GeneSet.\n       comts custom	To calculate single copy function enzyme gene community abandance by custom database.\n       comts stat	To generate a statistical table of differences and display the results graphically (Barplot and Heatmap).",
+  usage = "usage: comts geneset	To calculate community and RPKM abundance of GeneSet.\n       comts custom	To calculate single copy function enzyme gene community abandance by custom database.",
   add_help_option = TRUE,
   prog=NULL ,
   description = "This page is to show how to run the program.")
