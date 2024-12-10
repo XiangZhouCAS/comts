6f417a68e2551d432c8cd918b1b5679cf8d6fb56
XiangZhouCAS <147217990+XiangZhouCAS@users.noreply.github.com>
2024-12-10 11:29:49 +0800

    Update diamond_community.r

--- bins/diamond_community.r
+++ bins/diamond_community.r
@@ -21,7 +21,7 @@ option_list <- list(
   make_option(c("--filter_condition","-l"),type = "character",default = "default",
 	      help = "Please set the filter_condition file"),
   make_option(c("--keep_samples","-e"),type = "character",default = "keep",
-              help = "If you do not set 'keep', you can to delete these tmp results, default is keep."))
+              help = "If you do not set 'keep', you can delete these tmp results, default is keep."))
 opt_parser = OptionParser(
   usage = "usage: comts custom diy [options]",
   option_list = option_list,
