b723648b71ddf7f868d7f78bdf30b059fae1183e
XiangZhouCAS <147217990+XiangZhouCAS@users.noreply.github.com>
2024-12-18 15:55:03 +0800

    Update comts.help.r

--- bins/comts.help.r
+++ bins/comts.help.r
@@ -7,7 +7,7 @@ option_list <- list(
   make_option(c("--help","-h"),type = "character",default = F,
               help = "Show this help message and exit"))
 opt_parser = OptionParser(
-  usage = "usage: comts geneset	To calculate AFG and RPKM of GeneSet.\n       comts custom	To calculate single copy function enzyme AFG by customed databas.",
+  usage = "usage: comts geneset	To calculate AFG and RPKM of GeneSet.\n       comts custom	To calculate single copy function enzyme AFG by customed database.",
   add_help_option = TRUE,
   prog=NULL ,
   description = "This page is to show how to run the program.")
